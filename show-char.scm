f776d3fc252b86518b8940a59b0cc4429f688132
melvin <melvinzhang@gmail.com>
2013-11-25 15:07:54 +0800

    show char and int repreesntation on a single line

--- show-char.scm
+++ show-char.scm
@@ -1,11 +1,13 @@
 (define (loop) 
-  (let ((ch (read-char)))  
-    (display "char: ")
-    (write ch)
-    (newline)
-    (display "int: ")
-    (write (char->integer ch))
-    (newline)
-    (loop)))
+  (let ((ch (read-char)))
+    (if (eof-object? ch) #f
+      (begin
+        (display "char: ")
+        (write ch)
+        (display " ")
+        (display "int: ")
+        (write (char->integer ch))
+        (newline)
+        (loop)))))
 
 (loop)
